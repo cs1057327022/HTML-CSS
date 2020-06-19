e3bec866dc14e4ef2dd55c278894c7c011af8e2a
cs1057327022 <1057327022@qq.com>
2020-06-19 19:34:00 +0800

    全部htmlcss

---
+++ 0604/cs-npm/index.js
@@ -0,0 +1,13 @@
+let obj ={
+    name:'chen'
+}
+/*
+1会大部流程
+2总结：错误 发布博客
+3package.json 不能写注释 这个文件是核心文件
+任务发布成功
+*/
+
+console.log('chenshun is  fat');
+
+// module.export = obj
---
+++ 0610/封装自己的样式库/readmin.md
@@ -0,0 +1,12 @@
+## 按钮组件
+.btn 基础类
+状态
+ - .btn-info
+ - .btn-success
+大小
+ - .btn-sm
+ - .btn-md
+ - .btn-lg
+
+禁止点击
+ 
